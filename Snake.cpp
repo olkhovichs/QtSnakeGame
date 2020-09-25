720bd57df463e9b9c996f498c818f0cb219954ad
olkhovichs@icloud.com <olkhovichs@icloud.com>
2020-09-26 00:25:30 +0300

    the first concept whith png object

--- Snake.cpp
+++ Snake.cpp
@@ -1,7 +1,27 @@
 #include "Snake.h"
 
 Snake::Snake(QWidget *parent)
-    : QMainWindow(parent)
-{
+    : QMainWindow(parent) {
     ui.setupUi(this);
+
+    setStyleSheet("background-color:grey");
+    leftDirection = false;
+    rightDirection = true;
+    upDirection = false;
+    downDirection = false;
+    inGame = true;
+
+    resize(kWidth, kHeight);
+    setWindowTitle("Snake");
+    setWindowIcon(QIcon("titleIcon.png"));
+
+    loadImage();
 }
+
+void Snake::loadImage() {
+    dot->load("dot.png");
+    head->load("head.png");
+    fruit->load("fruit.png");
+}
+
+
--- Snake.h
+++ Snake.h
@@ -1,10 +1,13 @@
-#pra#pragma once
+#pragma once
 
 #include "ui_Snake.h"
 
 #include <QtWidgets/QMainWindow>
+#include <QKeyEvent>
+#include <QPainter>
 
 #include <vector>
+#include <string>
 
 namespace Ui {
     class Snake;
@@ -20,15 +23,44 @@ public:
 private:
     Ui::SnakeClass ui;
 
-    static const int kWidth = 500;
-    static const int kHeight = 500;
+    static const int kWidth = 1080;
+    static const int kHeight = 720;
     static const int kSizeDot = 10;
     static const int kAllDots = kWidth * kHeight;
     static const int kRandPosition = 20;
     static const int kDelay = 150;
 
+   // QWidget* centralWidget;
+
+    QImage* dot;
+    QImage* head;
+    QImage* fruit;
+    const QString& imgPath = "C:\VisualStudio\Qt\Snake\img";
+
+    int timerIs;
+    int dots;
+    int fruitX;
+    int fruitY;
+
+    bool leftDirection;
+    bool rightDirection;
+    bool upDirection;
+    bool downDirection;
+    bool inGame;
+
     std::vector<int> x;
     std::vector<int> y;
+
+    void loadImage();
+    void initGame();
+    void locateFruit();
+    void checkFruit();
+    void checkCollision();
+    void move();
+    void doDrawing();
+    void gameOver(QPainter&);
+
+    //void paintObject();
 
 
 };
