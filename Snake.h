b4720936c8310fc72811c25dbaadd297c271c692
olkhovichs@icloud.com <olkhovichs@icloud.com>
2020-09-28 01:10:09 +0300

    init

--- Snake.h
+++ Snake.h
@@ -2,68 +2,38 @@
 
 #include "ui_Snake.h"
 
-#include <QtWidgets/QMainWindow>
+#include <QMainWindow>
+#include <QLabel>
 #include <QKeyEvent>
-#include <QPainter>
-#include <QTime>
-
-#include <vector>
-#include <string>
 
 namespace Ui {
-    class Snake;
+	class Snake;
 }
 
 class Snake : public QMainWindow {
-
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit Snake(QWidget* parent = Q_NULLPTR);
+	explicit Snake(QWidget* parent = Q_NULLPTR);
+	~Snake();
+
+	static struct snakeNode {
+		QLabel* label;
+		int type;
+		int x, y;
+	};
 
 private:
-    Ui::SnakeClass ui;
+	void keyPressEvent(QKeyEvent* e);
 
-    void paintEvent(QPaintEvent*);
-    void timerEvent(QTimerEvent*);
-    void keyPressEvent(QKeyEvent*);
-
-    static const int kWidth = 1080;
-    static const int kHeight = 720;
-    static const int kSizeDot = 10;
-    static const int kAllDots = kWidth * kHeight;
-    static const int kRandPos = 20;
-    static const int kDelay = 150;
-
-   // QWidget* centralWidget;
-
-    QImage dot;
-    QImage head;
-    QImage fruit;
-    //const QString& imgPath = "C:\VisualStudio\Qt\Snake\img";
-
-    int timerId;
-    int snakeDots;
-    int fruitX;
-    int fruitY;
-
-    bool leftDirection;
-    bool rightDirection;
-    bool upDirection;
-    bool downDirection;
-    bool inGame;
-
-    std::vector<int> x;
-    std::vector<int> y;
-
-    void loadImage();
-    void initGame();
-    void locateFruit();
-    void checkFruit();
-    void checkCollision();
-    void move();
-    void doDrawing();
-    void gameOver(QPainter&);
-
-    //void paintObject();
+	void initGame();
+	void drawBorder();
+	void drawGameArea();
+	void showGameStatus();
+	void mainMenu();
+	void barMenu();
+	void moveSnake();
+	void checkCollision();
+	void createFruits();
+	void gameOver();
 };
